ce67dad6350c3900e6c6f843b9f50da3c8f1ee03
Kol Kheang <kol.kheang@cerner.com>
2019-09-23 19:59:23 -0500

    Limit patient listing to patient with smart family name

--- Sources/Client/PatientList.swift
+++ Sources/Client/PatientList.swift
@@ -26,7 +26,7 @@ public enum PatientListStatus: Int {
  *  You can use subscript syntax to safely retrieve a patient from the list: patientList[5]
  */
 open class PatientList {
-	
+
 	/// Current list status.
 	open var status: PatientListStatus = .unknown {
 		didSet {
@@ -35,10 +35,10 @@ open class PatientList {
 		}
 	}
 	fileprivate var lastStatusError: FHIRError? = nil
-	
+
 	/// A block executed whenever the receiver's status changes.
 	open var onStatusUpdate: ((FHIRError?) -> Void)?
-	
+
 	/// The patients currently in this list.
 	var patients: [Patient]? {
 		didSet {
@@ -48,52 +48,52 @@ open class PatientList {
 			onPatientUpdate?()
 		}
 	}
-	
+
 	/// A block to be called when the `patients` property changes.
 	open var onPatientUpdate: (() -> Void)?
-	
+
 	fileprivate(set) open var expectedNumberOfPatients: UInt = 0
-	
+
 	/// The number of patients currently in the list.
 	open var actualNumberOfPatients: UInt {
 		return UInt(patients?.count ?? 0)
 	}
-	
+
 	var sections: [PatientListSection] = []
-	
+
 	open var numSections: Int {
 		return sections.count
 	}
-	
+
 	open internal(set) var sectionIndexTitles: [String] = []
-	
+
 	/// How to order the list.
 	open var order = PatientListOrder.nameFamilyASC
-	
+
 	/// The query used to create the list.
 	public let query: PatientListQuery
-	
+
 	/// Indicating whether not all patients have yet been loaded.
 	open var hasMore: Bool {
 		return query.search.hasMore
 	}
-	
-	
+
+
 	public init(query: PatientListQuery) {
 		self.query = query
 		self.status = .initialized
 	}
-	
-	
+
+
 	// MARK: - Patients & Sections
-	
+
 	subscript(index: Int) -> PatientListSection? {
 		if sections.count > index {
 			return sections[index]
 		}
 		return nil
 	}
-	
+
 	/**
 	Create sections from our patients. On iOS we could use UILocalizedCollection, but it's cumbersome on
 	non-NSObject subclasses. Assumes that the patient list is already ordered
@@ -102,7 +102,7 @@ open class PatientList {
 		if let patients = self.patients {
 			sections = [PatientListSection]()
 			sectionIndexTitles = [String]()
-			
+
 			var n = 0
 			var lastTitle: Character = "$"
 			var lastSection = PatientListSection(title: "")
@@ -118,7 +118,7 @@ open class PatientList {
 				lastSection.add(patient: patient)
 				n += 1
 			}
-			
+
 			// not all patients fetched yet?
 			if actualNumberOfPatients < expectedNumberOfPatients {
 				let sham = PatientListSectionPlaceholder(title: "↓")
@@ -132,13 +132,13 @@ open class PatientList {
 			sectionIndexTitles = []
 		}
 	}
-	
-	
+
+
 	// MARK: - Patient Loading
-	
+
 	/**
 	Executes the patient query against the given FHIR server and updates the receiver's `patients` property when done.
-	
+
 	- parameter fromServer: A FHIRServer instance to query the patients from
 	*/
 	open func retrieve(fromServer server: FHIRServer) {
@@ -147,16 +147,16 @@ open class PatientList {
 		query.reset()
 		retrieveBatch(fromServer: server)
 	}
-	
+
 	/**
 	Attempts to retrieve the next batch of patients. You should check `hasMore` before calling this method.
-	
+
 	- parameter fromServer: A FHIRServer instance to retrieve the batch from
 	*/
 	open func retrieveMore(fromServer server: FHIRServer) {
 		retrieveBatch(fromServer: server, appendPatients: true)
 	}
-	
+
 	func retrieveBatch(fromServer server: FHIRServer, appendPatients: Bool = false) {
 		status = .loading
 		query.execute(onServer: server, order: order) { [weak self] bundle, error in
@@ -171,23 +171,23 @@ open class PatientList {
 				else {
 					var patients: [Patient]? = nil
 					var expTotal: Int32? = nil
-					
+
 					// extract patient resources from the search result bundle
 					if let bndle = bundle {
 						if let total = bndle.total?.int32 {
 							expTotal = total
 						}
-						
+
 						if let entries = bndle.entry {
 							let newPatients = entries
 								.filter() { $0.resource is Patient }
 								.map() { $0.resource as! Patient }
-							
+
 							let append = appendPatients && nil != this.patients
 							patients = this.order.ordered(append ? this.patients! + newPatients : newPatients)
 						}
 					}
-					
+
 					callOnMainThread() {
 						if let total = expTotal {
 							this.expectedNumberOfPatients = UInt(total)
@@ -210,11 +210,11 @@ open class PatientList {
 A patient list holding all available patients.
 */
 open class PatientListAll: PatientList {
-	
+
 	public init() {
-		let search = FHIRSearch(query: [])
+		let search = FHIRSearch(query: ["family": "smart"])
 		search.pageCount = 50
-		
+
 		super.init(query: PatientListQuery(search: search))
 	}
 }
@@ -225,27 +225,27 @@ Patients are divided into sections, e.g. by first letter of their family name. T
 to one section.
 */
 open class PatientListSection {
-	
+
 	open var title: String
 	var patients: [Patient]?
 	var numPatients: UInt {
 		return UInt(patients?.count ?? 0)
 	}
-	
+
 	/// How many patients are in sections coming before this one. Only valid in context of a PatientList.
 	open var offset: Int = 0
-	
+
 	public init(title: String) {
 		self.title = title
 	}
-	
+
 	func add(patient: Patient) {
 		if nil == patients {
 			patients = [Patient]()
 		}
 		patients!.append(patient)
 	}
-	
+
 	subscript(index: Int) -> Patient? {
 		if let patients = patients, patients.count > index {
 			return patients[index]
@@ -255,10 +255,9 @@ open class PatientListSection {
 }
 
 class PatientListSectionPlaceholder: PatientListSection {
-	
+
 	override var numPatients: UInt {
 		return holdingForNumPatients
 	}
 	var holdingForNumPatients: UInt = 0
 }
-
